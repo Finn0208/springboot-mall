afc8434511cb99c848e852c80f77ea024ec1c6b5
Finn0208 <rec520430@gmail.com>
2024-12-12 14:48:23 +0800

    商品功能 - 實作修改商品

--- src/main/java/com/finn/springbootmall/controller/ProductController.java
+++ src/main/java/com/finn/springbootmall/controller/ProductController.java
@@ -1,6 +1,5 @@
 package com.finn.springbootmall.controller;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.finn.springbootmall.dto.ProductRequest;
 import com.finn.springbootmall.model.Product;
 import com.finn.springbootmall.service.ProductService;
@@ -28,11 +27,29 @@ public class ProductController {
     }
 
     @PostMapping("/products")
-    public ResponseEntity<Product> createProduct(@RequestBody @Valid ProductRequest productRequest){
+    public ResponseEntity<Product> createProduct(@RequestBody @Valid ProductRequest productRequest) {
         Integer productId = productService.createProduct(productRequest);
 
         Product product = productService.getProductById(productId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest) {
+        // 先檢查 product 是否存在
+        Product product = productService.getProductById(productId);
+
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        // 修改商品數據
+        productService.updateProduct(productId, productRequest);
+
+        Product updatedProduct = productService.getProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
     }
 }
